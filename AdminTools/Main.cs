db0ea891b5c4d5ee4e5834d25c43f2409b513b72
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2025-06-06 21:23:53 +0200

    v bump

--- AdminTools/Main.cs
+++ AdminTools/Main.cs
@@ -31,7 +31,7 @@ namespace AdminTools
 		public override string Name { get; } = "Admin Tools";
 		public override string Prefix { get; } = "AdminTools";
 		public override PluginPriority Priority { get; } = PluginPriority.Highest;
-		public override Version Version { get; } = new(8, 0, 0);
+		public override Version Version { get; } = new(8, 0, 1);
         public override Version RequiredExiledVersion { get; } = new(8, 14, 1);
 
         public static Main Instance;
